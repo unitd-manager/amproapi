d4d2b74bb5759066e4e94954fd436659b2996859
syed jasmine <smsaf91@gmail.com>
2025-04-22 14:58:37 +0530

    first commit

--- config/Database.js
+++ config/Database.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 var db = mysql.createConnection({
     host: 'localhost',
-    user: 'foodecom',
-    password: '4b29e35a430fb8',
-    database: 'foodecom'
+    user: 'root',
+    password: '',
+    database: 'ampro'
 });
 db.connect(); 
 module.exports = db;
